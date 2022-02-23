8a40a0e7bd43ad0b5273d318fb84e5579fdad474
Karen Yen <karenyen@huntsman-ve703-00694.apn.wlan.private.upenn.edu>
2022-02-23 15:22:15 -0500

    test karen

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,6 +3,10 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
+
+/**
+ testing karen
+ */
 import { FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
