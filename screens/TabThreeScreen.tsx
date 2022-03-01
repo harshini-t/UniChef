c4df3360fb67edc1c884ad04bafac617cde6105d
vivianrchiang <vivianrc@seas.upenn.edu>
2022-03-01 18:10:57 -0500

    marketplace tab

---
+++ screens/TabThreeScreen.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+
+export default function TabThreeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile</Text>
+      <Text style={styles.title}>Kirby</Text>
+      <Text style={styles.title}>@kirby</Text>
+      <img src="https://upload.wikimedia.org/wikipedia/en/2/2d/SSU_Kirby_artwork.png" alt="Kirby" 
+      width="200" height="250"></img>{"\n"} {"\n"}
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabThreeScreen.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
