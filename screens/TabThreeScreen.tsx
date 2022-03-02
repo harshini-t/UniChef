d7949c6c4f0d3f13d7264364bd8f4fd8eb819004
Karen Yen <karenyen@vagelos-ve703-05421.apn.wlan.private.upenn.edu>
2022-03-01 21:39:52 -0500

    profile page update

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -1,36 +1,92 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity
+} from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+export default class Profile extends Component {
 
-export default function TabThreeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
-      <Text style={styles.title}>Kirby</Text>
-      <Text style={styles.title}>@kirby</Text>
-      <img src="https://upload.wikimedia.org/wikipedia/en/2/2d/SSU_Kirby_artwork.png" alt="Kirby" 
-      width="200" height="250"></img>{"\n"} {"\n"}
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabThreeScreen.tsx" />
-    </View>
-  );
+  render() {
+    return (
+      <View style={styles.container}>
+          <View style={styles.header}></View>
+          <Image style={styles.profile} source={{uri: 'https://upload.wikimedia.org/wikipedia/en/2/2d/SSU_Kirby_artwork.png'}}/>
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>Kirby</Text>
+              <Text style={styles.info}>@kirbyeats</Text>
+              <Text style={styles.info}>10k Followers / 100 Following / 2 Posts</Text>
+              <Text style={styles.bio}> Hi! I'm from Taiwan and I love eating and cooking Asian food. Check out my recipes!</Text>
+              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text style={styles.appButtonText}>Recipes</Text>
+              </TouchableOpacity>              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text style={styles.appButtonText}>Meals for Sale</Text>
+              </TouchableOpacity>
+            </View>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  header:{
+    backgroundColor: "#00008b",
+    height:200,
+  },
+  profile: {
+    width: 200,
+    height: 200,
+    borderRadius: 120,
+    borderWidth: 5,
+    borderColor: "#8b0000",
+    marginBottom:10,
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop:130
+  },
+  body:{
+    marginTop:100,
+  },
+  bodyContent: {
     flex: 1,
     alignItems: 'center',
+    padding:30,
+  },
+  name:{
+    fontSize:28,
+    color: "#00008b",
+    fontWeight: "600"
+  },
+  info:{
+    fontSize:16,
+    color: "#696969",
+    marginTop:10
+  },
+  bio:{
+    fontSize:16,
+    color: "#00008b",
+    marginTop:10,
+    textAlign: 'center'
+  },
+  buttonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#00008b",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  appButtonText: {
+    fontSize: 14,
+    color: "#ffffff",
+  }
 });
