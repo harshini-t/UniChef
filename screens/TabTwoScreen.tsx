81760006e3f10b1ed91c4eb7817646683c4dd21e
DESKTOP-3ST4AD6 <mpriya946@gmail.com>
2022-03-02 15:03:54 -0500

    some more changes to recipe feed, reviews button

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -27,12 +27,11 @@ export default function TabTwoScreen() {
                 </View>
             </View>
 
-        </View>
+      </View>
       
       
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+
     </View>
   );
 }
